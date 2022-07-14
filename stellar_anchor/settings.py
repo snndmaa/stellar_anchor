47861326fcddb2ead850772099473602ca68f7a9
sndmaa <sanclaw55@gmail.com>
2022-07-14 15:45:12 +0100

    requirements.txt

--- stellar_anchor/settings.py
+++ stellar_anchor/settings.py
@@ -32,6 +32,27 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
