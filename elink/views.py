dbc7c5d2e6ce97b42ad6047a2117d2e045974df7
sndmaa <sanclaw55@gmail.com>
2022-07-21 11:34:21 +0100

    before usermodel

--- elink/views.py
+++ elink/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import ElinkStellarAccount
 
 # Create your views here.
+
+def user_for_account(request, account_id):
+    return ElinkStellarAccount.objects.get(account=account_id) or ElinkStellarAccount.objects.get(memo=account_id)
+
+def create_user(request):
+    pass
